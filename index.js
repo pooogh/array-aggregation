032484c71edc7a141e806c624f6b3dfbdc09b5a8
Lucy <pooogh9@gmail.com>
2024-03-22 14:22:32 +0300

    check v2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const tableParsing = (/* content */) => {
 
 // task 2
 const candidateAssessment = (/* content */) => {
-
+    console.log('Job seeker: John Doe, Junior JavaScript Developer');
 };
 
 // task 3
